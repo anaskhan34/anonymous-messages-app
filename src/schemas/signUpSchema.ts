5f8f7d829224ef50a5e772e3b025d13806476d0d
Muhammad Anas <anaskha3451@gmail.com>
2026-09-07 13:24:51 +0500

    unique username check is done and otp verification is done

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const usernameSchema = z
+export const usernameValidation = z
   .string()
   .min(3, "Username must be at least 3 characters")
   .max(20, "Username must be at most 20 characters")
